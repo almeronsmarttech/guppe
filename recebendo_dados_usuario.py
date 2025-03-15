1482eeffa1fb0f10d5fc6daef69fd44c0e2717be
Almeron SmartTech <almeron@almeronsmarttech.com.br>
2025-03-15 19:28:08 -0300

    Terceiro Commit

--- recebendo_dados_usuario.py
+++ recebendo_dados_usuario.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 
 nome = input("Digite o seu nome: ")
-idade = input("Digite a sua idade: ")
+idade = int(input("Digite a sua idade: "))
 
 print(f"Seja muito bem-vindo(a) {nome}!")
 print(f"Sua idade é {idade} anos.")
+
+print(f"Olá {nome}, você tem {idade} anos de idade. Nasceu em {datetime.now().year-idade}")
